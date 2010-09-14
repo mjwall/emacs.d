ffc6d9db7bcb27b501ba7f05a7c755707c891dd8
Michael Wall <mjwall@gmail.com>
2010-09-14 16:12:33 -0400

    Add twilight theme

--- mwall/theme.el
+++ mwall/theme.el
@@ -1,5 +1,5 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 
-;;(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
-;;(color-theme-twilight)
+(load-file (concat dotfiles-dir "/vendor/themes/twilight-emacs/color-theme-twilight.el"))
+(color-theme-twilight)

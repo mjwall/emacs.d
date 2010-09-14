cf9b0bb4213d3b5c500940cc6e1bf036127a0527
Michael Wall <mjwall@gmail.com>
2010-09-14 16:03:04 -0400

    Add color-themes.  Now to use defunkt choice, twilight

--- mwall.el
+++ mwall.el
@@ -4,9 +4,13 @@
 
 (message "Loading mwall.el")
 
+;; add vendor to load-path
+(add-to-list 'load-path  (concat dotfiles-dir "/vendor"))
+
 (load "mwall/global")
 (load "mwall/defuns")
 (load "mwall/binding")
+(load "mwall/theme")
 ;;(load "mwall/modes")
 ;;(load "mwall/theme")
 ;; html, ruby, clojure, paredit, php, nxml, yaml, perl, markdown, javadoc, javascript, css  ?
---
+++ mwall/theme.el
@@ -0,0 +1,5 @@
+(require 'color-theme)
+(setq color-theme-is-global t)
+
+;;(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
+;;(color-theme-twilight)

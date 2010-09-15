a2ec0334eaa79669859504857218e18f38d8ceb4
Michael Wall <mjwall@gmail.com>
2010-09-15 09:44:02 -0400

    Redo the bindings a little.  Didn't realize enabling default window movement was shift plus direction.  Added a few more bindings from ESK

--- mwall/binding.el
+++ mwall/binding.el
@@ -13,6 +13,11 @@
   (global-set-key (kbd "C-x C-b") 'ibuffer))
 (when (fboundp 'recentf-mode)
   (global-set-key (kbd "M-<f12>") 'recentf-open-files)
+  (global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
+  (global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+  (global-set-key (kbd "C-c y") 'bury-buffer)
+  (global-set-key (kbd "C-c r") 'revert-buffer)
+  (global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
   (global-set-key (kbd "C-x f") 'recentf-ido-find-file))
 
 ;; Window switching, TODO: figure out what this is
@@ -28,20 +33,15 @@
 (global-set-key "\r" 'newline-maybe-indent) ;;instead of built in newline-and-indent
 (global-set-key (kbd "C-;") 'comment-region)
 
+
 ;; no mail
 (global-unset-key (kbd "C-x m"))
 
 ;; rebind to undo, stop suspending-frame
 (global-set-key (kbd "C-z") 'undo)
 
-;; add binding for switch window C-x o
-(global-set-key [C-tab] 'other-window)
-
 ;;TODO: These need to moved out.  This file should not contain bindings for
 ;; additional packages.  What to do with bindings for custom functions though
 ;; fullscreen
 (global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
 
-;; emacs-nav
-(when (fboundp 'nav)
-  (global-set-key (kbd "C-c n") 'nav-toggle))
--- mwall/emacs-nav.el
+++ mwall/emacs-nav.el
@@ -1,3 +1,7 @@
 ;; add emacs-nav, cloned from hg clone https://emacs-nav.googlecode.com/hg/ emacs-nav
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/emacs-nav"))
 (require 'nav)
+
+;; emacs-nav
+(when (fboundp 'nav)
+  (global-set-key (kbd "C-c n") 'nav-toggle))

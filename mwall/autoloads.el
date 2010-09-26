dccecd684768c10e864566522fa0a951a809e68d
Michael Wall <mjwall@gmail.com>
2010-09-25 20:17:33 -0400

    Install php mode, appears to work, but only opened a php file to see it load

--- mwall/autoloads.el
+++ mwall/autoloads.el
@@ -59,3 +59,8 @@
      (setq interpreter-mode-alist (append '(("groovy" . groovy-mode))
                                           interpreter-mode-alist))
 (require 'grails-mode) ;;this doesn't seem right
+
+;; php-mode
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))

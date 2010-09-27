82a0aeca00b673f17f2eb02a31ef8f0e6f9cf6b0
Michael Wall <mjwall@gmail.com>
2010-09-27 14:24:38 -0400

    Add tangotango theme.  Almost zenburn, but with sharper text

--- mwall/theme.el
+++ mwall/theme.el
@@ -2,10 +2,13 @@
 (setq color-theme-is-global t)
 
 (load-file (concat dotfiles-dir "/vendor/themes/twilight-emacs/color-theme-twilight.el"))
-(color-theme-twilight)
+;;(color-theme-twilight)
 
 (load-file (concat dotfiles-dir "/vendor/themes/color-theme-vivid-chalk.el"))
 ;;(color-theme-vivid-chalk)
 
 (load-file (concat dotfiles-dir "vendor/themes/zenburn.el"))
 ;;(color-theme-zenburn)
+
+(load-file (concat dotfiles-dir "vendor/themes/tangotango/color-theme-tangotango.el"))
+(color-theme-tangotango)

502ca31753d3ea7bb493b31026781bdadc8fda33
Michael Wall <mjwall@gmail.com>
2010-09-30 14:47:46 -0400

    Add eproject instead of project root.  Still needs some tweaking, but appears to be working.  Would like to have ack

--- mwall/project.el
+++ mwall/project.el
@@ -1,37 +1,13 @@
-;; project-root
+;; eproject
+(add-to-list 'load-path (concat dotfiles-dir "vendor/eproject"))
+(require 'eproject)
+(require 'eproject-extras)
 
-(add-to-list 'load-path (concat dotfiles-dir "vendor/project-root"))
+(define-project-type maven (generic) (look-for "pom.xml")
+  :relevant-files ("\\.java" "\\.html" "\\.jsp" "\\.css" "\\.js")
+  :irrelevant-files ("target/"))
+(define-project-type emacs.d (generic)
+  (and (look-for "init.el") (look-for "vendor"))
+  :relevant-files ("\\.el" "README" "\\.txt")
+  :irrelevant-files ("auto-save-list/" "backups/"))
 
-(require 'project-root)
-
-(setq project-roots
-       `(("Generic Maven Project"
-          :root-contains-files ("pom.xml" "src")
-          :filename-regex ,(regexify-ext-list '(java jsp html js css htm))
-          ;;:on-hit (lambda (p) (message (car p)))
-          ;;:exclude-paths ("target")
-          )
-         ("Grails Project"
-          :root-contains-files ("grails-app")
-          :filename-regex ,(regexify-ext-list '(groovy gsp java html css js))
-          ;;:exclude-paths ("target")
-          )
-         ("Clojure Lein Project"
-          :root-contains-files ("project.clj")
-          :filename-regex ,(regexify-ext-list '(clj html css js))
-          ;;:exclude-paths ("classes")
-          )
-         ("Emacs.d config"
-          :root-contains-files ("init.el" "vendor")
-          :filename-regex ,(regexify-ext-list '(el txt README ))
-          ;;:exclude-paths ("auto-save-list" "semanticdb" "backups")
-          )))
-
-;; keybindings
-(global-set-key (kbd "C-c p f") 'project-root-find-file)
-(global-set-key (kbd "C-c p g") 'project-root-grep)
-(global-set-key (kbd "C-c p a") 'project-root-ack)
-(global-set-key (kbd "C-c p d") 'project-root-goto-root)
-(global-set-key (kbd "C-c p p") 'project-root-run-default-command)
-(global-set-key (kbd "C-c p l") 'project-root-browse-seen-projects)
-(global-set-key (kbd "C-c p M-x") 'project-root-execute-extended-command)

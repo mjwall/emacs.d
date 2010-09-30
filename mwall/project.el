6bc8a6e7bb29064eb08f13a84ea9eba846effcf6
Michael Wall <mjwall@gmail.com>
2010-09-30 10:35:00 -0400

    Remove mk-project.  Too frustrating having to add for each project and make new directories for TAGS etc

--- mwall/project.el
+++ mwall/project.el
@@ -1,16 +1,1 @@
 ;; mk-project
-(add-to-list 'load-path (concat dotfiles-dir "vendor/mk-project"))
-(require 'mk-project)
-(global-set-key (kbd "C-c p c") 'project-compile)
-(global-set-key (kbd "C-c p l") 'project-load)
-(global-set-key (kbd "C-c p a") 'project-ack)
-(global-set-key (kbd "C-c p g") 'project-grep)
-(global-set-key (kbd "C-c p o") 'project-multi-occur)
-(global-set-key (kbd "C-c p u") 'project-unload)
-(global-set-key (kbd "C-c p f") 'project-find-file-ido) ; or project-find-file
-(global-set-key (kbd "C-c p i") 'project-index)
-(global-set-key (kbd "C-c p s") 'project-status)
-(global-set-key (kbd "C-c p h") 'project-home)
-(global-set-key (kbd "C-c p d") 'project-dired)
-(global-set-key (kbd "C-c p t") 'project-tags)
-(load "my-projects")

4ef64cece5ad132b397e2d4560b0db7f2f814e9c
Michael Wall <mjwall@gmail.com>
2010-09-30 10:25:31 -0400

    Add malabar 1.4.0.  Not sure where the srecode-mode.el came from, but looks like it is needed.  I don't recall adding it though.

---
+++ srecode-map.el
@@ -0,0 +1,7 @@
+;; Object SRecode Map
+;; EIEIO PERSISTENT OBJECT
+(srecode-map "SRecode Map"
+  :file "srecode-map.el"
+  :files '(("/Applications/Emacs.app/Contents/Resources/etc/srecode/wisent.srt" . wisent-grammar-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/texi.srt" . texinfo-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/template.srt" . srecode-template-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/make.srt" . makefile-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/java.srt" . java-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/el.srt" . emacs-lisp-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/default.srt" . default) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/cpp.srt" . c++-mode))
+  :apps '((tests ("/Applications/Emacs.app/Contents/Resources/etc/srecode/test.srt" . srecode-template-mode)) (getset ("/Users/mwall@texeltek.com/.emacs.d/vendor/malabar-1.4.0/lisp/srecode/srecode-getset-java.srt" . java-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/getset-cpp.srt" . c++-mode)) (ede ("/Applications/Emacs.app/Contents/Resources/etc/srecode/ede-make.srt" . makefile-mode)) (document ("/Applications/Emacs.app/Contents/Resources/etc/srecode/doc-java.srt" . java-mode) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/doc-default.srt" . default) ("/Applications/Emacs.app/Contents/Resources/etc/srecode/doc-cpp.srt" . c++-mode)))
+  )

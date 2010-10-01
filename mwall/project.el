e4f0e3f94306a17de4e47e04694f77acbff67331
Michael Wall <mjwall@gmail.com>
2010-09-30 22:48:01 -0400

    Add grails project definition

--- mwall/project.el
+++ mwall/project.el
@@ -4,10 +4,13 @@
 (require 'eproject-extras)
 
 (define-project-type maven (generic) (look-for "pom.xml")
-  :relevant-files ("\\.java" "\\.html" "\\.jsp" "\\.css" "\\.js")
+  :relevant-files ("\\.java" "\\.html" "\\.jsp" "\\.css" "\\.js" "\\.xml")
   :irrelevant-files ("target/"))
 (define-project-type emacs.d (generic)
   (and (look-for "init.el") (look-for "vendor"))
   :relevant-files ("\\.el" "README" "\\.txt")
   :irrelevant-files ("auto-save-list/" "backups/"))
+(define-project-type grails (generic) (look-for "grails-app")
+  :relevant-files ("\\.groovy" "\\.gsp" "\\.java" "\\.html" "\\.jsp" "\\.css" "\\.js" "\\.xml")
+  :irrelevant-files ("target"))
 

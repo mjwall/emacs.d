5aa4dc4c28e3b1606751d48fd07a0d150eab46d6
Michael Wall <mjwall@gmail.com>
2010-11-05 13:06:42 -0400

    oops, missed the shortcuts

--- mwall/mydired.el
+++ mwall/mydired.el
@@ -17,19 +17,11 @@
         ;; it's not loaded yet, so add our bindings to the load-hook
         (add-hook 'dired-load-hook 'my-dired-init))
 
-(defun my-dired-init ()
-        "Bunch of stuff to run for dired, either immediately or when it's
-         loaded."
-        ;; <add other stuff here>
-        (define-key dired-mode-map [return] 'joc-dired-single-buffer)
-        (define-key dired-mode-map [mouse-1] 'joc-dired-single-buffer-mouse)
-        (define-key dired-mode-map "^"
-              (function
-               (lambda nil (interactive) (joc-dired-single-buffer "..")))))
-
-      ;; if dired's already loaded, then the keymap will be bound
-      (if (boundp 'dired-mode-map)
-              ;; we're good to go; just add our bindings
-              (my-dired-init)
-        ;; it's not loaded yet, so add our bindings to the load-hook
-        (add-hook 'dired-load-hook 'my-dired-init))
+(global-set-key [(f5)] 'joc-dired-magic-buffer)
+      (global-set-key [(control f5)] (function
+              (lambda nil (interactive)
+              (joc-dired-magic-buffer default-directory))))
+      (global-set-key [(shift f5)] (function
+              (lambda nil (interactive)
+              (message "Current directory is: %s" default-directory))))
+      (global-set-key [(meta f5)] 'joc-dired-toggle-buffer-name)

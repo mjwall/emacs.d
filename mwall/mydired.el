67bcd8834052b718c061804300bb452834664349
Michael Wall <mjwall@gmail.com>
2010-11-05 13:28:35 -0400

    f5 already bound to isearch something, so I bound to f6

--- mwall/mydired.el
+++ mwall/mydired.el
@@ -17,11 +17,11 @@
         ;; it's not loaded yet, so add our bindings to the load-hook
         (add-hook 'dired-load-hook 'my-dired-init))
 
-(global-set-key [(f5)] 'joc-dired-magic-buffer)
-      (global-set-key [(control f5)] (function
-              (lambda nil (interactive)
-              (joc-dired-magic-buffer default-directory))))
-      (global-set-key [(shift f5)] (function
-              (lambda nil (interactive)
-              (message "Current directory is: %s" default-directory))))
-      (global-set-key [(meta f5)] 'joc-dired-toggle-buffer-name)
+(global-set-key (kbd "<f6>") 'joc-dired-magic-buffer)
+(global-set-key [C-f6] (function
+      (lambda nil (interactive)
+      (joc-dired-magic-buffer default-directory))))
+(global-set-key [S-f6] (function
+      (lambda nil (interactive)
+      (message "Current directory is: %s" default-directory))))
+(global-set-key [M-f6] 'joc-dired-toggle-buffer-name)

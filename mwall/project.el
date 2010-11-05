d7d478bc536481963b4d344b72e6f89067d79dcd
Michael Wall <mjwall@gmail.com>
2010-11-05 15:41:17 -0400

    Seems like project root is working.  Not sure when seen projects get saved though

--- mwall/project.el
+++ mwall/project.el
@@ -1,14 +1,36 @@
-(define-project-type maven (generic) (look-for "pom.xml")
-  :relevant-files ("\\.java" "\\.html" "\\.jsp" "\\.css" "\\.js" "\\.xml")
-  :irrelevant-files ("target/"))
-(define-project-type emacs.d (generic)
-  (and (look-for "init.el") (look-for "vendor"))
-  :relevant-files ("\\.el" "README" "\\.txt")
-  :irrelevant-files ("auto-save-list/" "backups/"))
-(define-project-type grails (generic) (look-for "grails-app")
-  :relevant-files ("\\.groovy" "\\.gsp" "\\.java" "\\.html" "\\.jsp" "\\.css" "\\.js" "\\.xml")
-  :irrelevant-files ("target"))
-(define-project-type clojure (generic) (look-for "project.clj")
-  :relevant-files ("\\.clj" "\\.jsp" "\\.css" "\\.js" "\\.xml")
-  :irrelevant-files ("lib/" "classes/"))
+(require 'ack)
+(require 'project-root)
 
+(setq project-roots
+      `(
+        ("Maven project"
+          :root-contains-files ("pom.xml")
+          ;;:filename-regex ,(regexify-ext-list '(java html groovy jsp js css xml))
+;;          :on-hit (lambda (p) (message (car p)))
+          ;;:exclude-paths ("target")
+          )
+         ("Clojure project"
+          :root-contains-files ("project.clj")
+          ;;:filename-regex ,(regexify-ext-list '(clj jsp css js xml html))
+          ;;:exclude-paths ("lib" "classes")
+          )
+         ("Grails project"
+          :root-contains-files ("grails-app")
+          ;;:filename-regex ,(regexify-ext-list '(groovy gsp java html jsp css js xml))
+          ;;:exclude-paths ("target")
+          )
+         (".emacs "
+          :root-contains-files ("init.el")
+          ;;:filename-regex ,(regexify-ext-list '(el org README txt))
+          ;;:exclude-paths ("backups" "autosave-list" "semanticdb")
+          )
+         )
+)
+
+
+(global-set-key (kbd "C-c p f") 'project-root-find-file)
+(global-set-key (kbd "C-c p g") 'project-root-grep)
+(global-set-key (kbd "C-c p a") 'project-root-ack)
+(global-set-key (kbd "C-c p d") 'project-root-goto-root)
+(global-set-key (kbd "C-c p p") 'project-root-run-default-command)
+(global-set-key (kbd "C-c p l") 'project-root-browse-seen-projects)

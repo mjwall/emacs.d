ba5888b98f4643d2e0e59f602c9f5a2a56bc977a
Michael Wall <mjwall@gmail.com>
2011-02-03 16:22:01 -0500

    Add Isolate file to ruby

--- init-ruby.el
+++ init-ruby.el
@@ -1,6 +1,7 @@
 (add-to-list 'auto-mode-alist '("buildfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Isolate" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.autotest" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile" . ruby-mode))
 (add-hook 'ruby-mode-hook 'run-coding-hook)

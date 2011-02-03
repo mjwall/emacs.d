64906d04dd0a87df127ab0fbb9e3633ed0a8fd87
Michael Wall <mjwall@gmail.com>
2011-02-03 16:21:09 -0500

    Change todo capture

--- init-org-mode.el
+++ init-org-mode.el
@@ -15,8 +15,10 @@
 
 ; org mobile
 (setq org-mobile-directory "~/Dropbox/MobileOrg")
-(setq org-mobile-inbox-for-pull "~/org/inbox.org")
+(setq org-mobile-inbox-for-pull "~/org/todo.org")
 (setq org-startup-indented t)
+(add-hook 'after-init-hook 'org-mobile-pull)
+(add-hook 'kill-emacs-hook 'org-mobile-push)
 
 ; refile to other files
 (setq org-outline-path-complete-in-steps t)
@@ -25,7 +27,7 @@
 
 ; capture
  (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/todo.org" "Uncategorized")
+      '(("t" "Todo" entry (file "~/org/todo.org" )
              "* TODO %?\n  %i\n  %a")
         ("j" "Journal" entry (file+datetree "~/org/journal.org")
          "* %?\nEntered on %U\n  %i\n  %a")))

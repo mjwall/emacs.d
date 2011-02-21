fb81dd1512340790a8f75c764cd32436186ea3f0
Michael Wall <mjwall@gmail.com>
2011-02-21 08:57:35 -0500

    Add rake filetype

--- init-ruby.el
+++ init-ruby.el
@@ -1,6 +1,7 @@
 (add-to-list 'auto-mode-alist '("buildfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Isolate" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.autotest" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile" . ruby-mode))

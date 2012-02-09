e055caffaacfcccda0f36bd33d290801782d7e63
Michael Wall <mjwall@gmail.com>
2012-02-08 20:03:02 -0500

    Add C-xC-m shortcut for smex to ido-extras

--- init-ido-extras.el
+++ init-ido-extras.el
@@ -7,5 +7,10 @@
 (require 'smex)
 (smex-initialize)
 (global-set-key "\M-x" 'smex)
+;; avoid M-x if possible, https://sites.google.com/site/steveyegge2/effective-emacs
+;;(global-set-key "\C-x\C-m" 'execute-extended-command)
+;;(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-x\C-m" 'smex)
+(global-set-key "\C-c\C-m" 'smex)
 
 (provide 'init-ido-extras)

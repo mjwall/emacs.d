554835fd06c3861ed00cee3ad42649ac72e44496
Michael Wall <mjwall@gmail.com>
2019-05-22 09:25:20 -0400

    fix term

--- custom.el
+++ custom.el
@@ -12,24 +12,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(ansi-color-names-vector
-     ["#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
-  '(ansi-term-color-vector
-     [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"] t)
   '(custom-safe-themes
-     (quote
-       ("9eaf8bc0c4f4143eb8bd1b2b425a1f0c18c0f9ecc4b79c2e250b5ff1031b3960" "03f28a4e25d3ce7e8826b0a67441826c744cbf47077fb5bc9ddb18afe115005f" "ae6d87b0e931c6671559f217d2369711b42fe23912f5c0de2da8d7da8e25be4d" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "e4a7a2a1d9224751785732a33cc00debfa020ad65d02758bf916f1a8676e7310" "ace9f12e0c00f983068910d9025eefeb5ea7a711e774ee8bb2af5f7376018ad2" "75bc4eb26434bbb4544db3e81a12acfc84d822ed0fd0706a42fa646089891043" "ec5f697561eaf87b1d3b087dd28e61a2fc9860e4c862ea8e6b0b77bd4967d0ba" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "2a7beed4f24b15f77160118320123d699282cbf196e0089f113245d4b729ba5d" default)))
- '(fci-rule-character-color "#d9d9d9")
- '(fci-rule-color "#d9d9d9" t)
- '(jdee-server-dir (expand-file-name "src/jdee-server" user-emacs-directory))
+    (quote
+      ("9eaf8bc0c4f4143eb8bd1b2b425a1f0c18c0f9ecc4b79c2e250b5ff1031b3960" "03f28a4e25d3ce7e8826b0a67441826c744cbf47077fb5bc9ddb18afe115005f" "ae6d87b0e931c6671559f217d2369711b42fe23912f5c0de2da8d7da8e25be4d" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "e4a7a2a1d9224751785732a33cc00debfa020ad65d02758bf916f1a8676e7310" "ace9f12e0c00f983068910d9025eefeb5ea7a711e774ee8bb2af5f7376018ad2" "75bc4eb26434bbb4544db3e81a12acfc84d822ed0fd0706a42fa646089891043" "ec5f697561eaf87b1d3b087dd28e61a2fc9860e4c862ea8e6b0b77bd4967d0ba" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "2a7beed4f24b15f77160118320123d699282cbf196e0089f113245d4b729ba5d" default)))
+  '(fci-rule-character-color "#d9d9d9")
+  '(fci-rule-color "#d9d9d9" t)
+  '(jdee-server-dir (expand-file-name "src/jdee-server" user-emacs-directory))
   '(org-file-apps
-     (quote
-       ((auto-mode . emacs)
-         ("\\.mm\\'" . default)
-         ("\\.x?html?\\'" . default)
-         ("\\.pdf\\'" . "evince %s"))))
- '(org-journal-dir "/home/mikewall/org/journal")
- '(org-journal-file-format "%Y%m%d.org")
+    (quote
+      ((auto-mode . emacs)
+        ("\\.mm\\'" . default)
+        ("\\.x?html?\\'" . default)
+        ("\\.pdf\\'" . "evince %s"))))
+  '(org-journal-dir "/home/mikewall/org/journal")
+  '(org-journal-file-format "%Y%m%d.org")
   '(package-selected-packages
      (quote
        (julia-mode ace-window pyenv-mode unfill cython-mode smex virtualenvwrapper company-anaconda web-mode ein inf-ruby py-autopep8 rvm company-go eshell-bookmark org-plus-contrib groovy-mode lua-mode kotlin-mode markdown-mode clojure-mode scala-mode flycheck-gradle cmake-mode protobuf-mode thrift docker-tramp docker-compose-mode dockerfile-mode ess gotest go-eldoc go-rename go-guru go-mode anaconda-mode tide ng1-mode typescript-mode json-mode js2-mode autodisass-java-bytecode jdee flycheck company counsel swiper ivy diminish use-package-ensure-system-package org-journal use-package ng2-mode))))
